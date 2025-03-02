825e5968debca91874608740ea42a94dffd0ed4a
Shyam sunder <129372271+Shyamsaini890@users.noreply.github.com>
2025-03-02 17:32:08 +0530

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpBc2OLxsYrC-DAucAViuCZsjfR2RZOFY",
-  authDomain: "task4-ff1b4.firebaseapp.com",
-  projectId: "task4-ff1b4",
-  storageBucket: "task4-ff1b4.appspot.com",
-  messagingSenderId: "576053025365",
-  appId: "1:576053025365:web:a76293d4f242549aa212dc",
+
+
+// Write Your own Firebase api key and all credential
+
+
+  
 };
 
 const app = initializeApp(firebaseConfig);
